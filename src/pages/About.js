35bfabc97e3b9c07c17222283e05e80ba674b924
Sanjyot Amritkar <sanjyotamritkar@gmail.com>
2025-11-02 13:43:15 -0500

    Fixed unused variable Skills in About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,55 +9,6 @@ import {
 } from 'react-icons/si';
 
 function About() {
-  
-  const skills = {
-    "Languages & Frameworks": [
-      { name: 'Java', icon: <FaJava /> },
-      { name: 'Python', icon: <FaPython /> },
-      { name: 'JavaScript', icon: <SiJavascript /> },
-      { name: 'React.js', icon: <FaReact /> },
-      { name: 'Node.js', icon: <FaNodeJs /> },
-      { name: 'HTML/CSS', icon: <SiJavascript /> },
-      { name: 'Springboot', icon: <SiSpringboot /> }
-    ],
-    "Tools & Platforms": [
-      { name: 'Git', icon: <FaDocker /> },
-      { name: 'Jenkins', icon: <FaDocker /> },
-      { name: 'VS Code', icon: <SiJavascript /> },
-      { name: 'Jupyter', icon: <SiJavascript /> },
-      { name: 'Power BI', icon: <SiJavascript /> },
-      { name: 'Tableau', icon: <SiJavascript /> },
-      { name: 'WEKA', icon: <SiJavascript /> },
-      { name: 'Jira', icon: <SiJavascript /> },
-      { name: 'Openshift', icon: <SiJavascript /> }
-    ],
-    "Databases": [
-      { name: 'MySQL', icon: <SiPostgresql /> },
-      { name: 'PostgreSQL', icon: <SiPostgresql /> },
-      { name: 'MongoDB', icon: <SiMongodb /> },
-      { name: 'Oracle', icon: <SiPostgresql /> },
-      { name: 'Firebase', icon: <SiJavascript /> },
-      { name: 'AWS', icon: <FaAws /> },
-      { name: 'H2', icon: <SiJavascript /> }
-    ],
-    "Data Science & ML": [
-      { name: 'Pandas', icon: <SiPandas /> },
-      { name: 'NumPy', icon: <SiJavascript /> },
-      { name: 'Scikit-learn', icon: <SiScikitlearn /> },
-      { name: 'Keras', icon: <SiJavascript /> },
-      { name: 'Seaborn', icon: <SiJavascript /> },
-      { name: 'TensorFlow', icon: <SiTensorflow /> },
-      { name: 'Flask', icon: <SiJavascript /> },
-      { name: 'Matplotlib', icon: <SiJavascript /> },
-      { name: 'Plotly', icon: <SiJavascript /> },
-      { name: 'Docker', icon: <FaDocker /> },
-      { name: 'Kubernetes', icon: <SiKubernetes /> },
-      { name: 'D3.js', icon: <SiJavascript /> },
-      { name: 'ETL', icon: <SiJavascript /> },
-      { name: 'LLMs', icon: <SiJavascript /> },
-      { name: 'RAG', icon: <SiJavascript /> }
-    ]
-  };
 
   return (
     <section id="about" className="about-section">
